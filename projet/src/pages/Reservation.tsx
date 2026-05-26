a0767e09dff2c92488c66c8739628920a7f09dc9
Joe-Wando <jonathan6wando@gmail.com>
2026-05-26 23:44:31 +0000

    Ajout de message de confirmation de reservation avec useState

--- projet/src/pages/Reservation.tsx
+++ projet/src/pages/Reservation.tsx
@@ -1,12 +1,28 @@
 import { useParams } from "react-router-dom"
+import films from "../films" 
+import { useState } from "react"
 
 export default function Reservation() {
   const { id } = useParams()
+const film = films.find(function(f) {
+  return f.id === Number(id)
+})
+const [reserve, setReserve] = useState(false)
+function confirmerReservation() {
+  setReserve(true)
+}
 
   return (
-    <div>
-      <h1>Vos réservations</h1>
-      <p>Vous avez choisi le film numéro : {id}</p>
-    </div>
+<div><h1>Réservation</h1>
+<p>Vous avez choisi : {film?.titre}</p>
+
+{reserve ? (
+  <p>Réservation confirmée !</p>
+) : (
+  <button onClick={confirmerReservation}>
+    Confirmer la réservation
+  </button>
+)}</div>
+
   )
 }
