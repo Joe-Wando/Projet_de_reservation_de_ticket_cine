c78dfd07199c02477a8cc66903db49123c475830
Joe-Wando <jonathan6wando@gmail.com>
2026-05-27 02:29:30 +0000

    Fin de l'instauration de tailwind css

--- projet/src/pages/Acceuil.tsx
+++ projet/src/pages/Acceuil.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 export default function Acceuil(){
 return (
      <div>
-       <h1>Bienvenu sur SENECINE</h1>
+       <h1 className="text-red-500">Bienvenu sur SENECINE</h1>
         <p>Chez nous, vous trouverez les meilleurs films et séries et n'oubliez de faire votre reservation à l'avance.</p>
         <Link to="/Films">Voir les films</Link>
      </div>
