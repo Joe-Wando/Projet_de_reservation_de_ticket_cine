80c80a63461928ac4b6cd693381a09b7ecd3a63e
Joe-Wando <jonathan6wando@gmail.com>
2026-05-27 17:18:28 +0000

    connexion de la page connexion à la page App

--- projet/src/App.tsx
+++ projet/src/App.tsx
@@ -3,6 +3,8 @@ import Acceuil from './pages/Acceuil'
 import Films from './pages/Films'
 import Reservation from './pages/Reservation'
 import Inscription from './pages/Inscription'
+import Connexion from './pages/Connexion'
+
 export default function App(){
   return(
       <Routes>
@@ -10,6 +12,7 @@ export default function App(){
         <Route path="/Films" element={<Films />}/>
         <Route path="/reservation/:id" element={<Reservation />} />   
       <Route path="/inscription" element={<Inscription />} />
+<Route path="/connexion" element={<Connexion />} />
       </Routes>
 
 )
--- projet/src/pages/Connexion.tsx
+++ projet/src/pages/Connexion.tsx
@@ -6,9 +6,31 @@ export default function Connexion() {
   const [email, setEmail] = useState("")
   const [motDePasse, setMotDePasse] = useState("")
 
-  return (
-    <div>
-      <h1>Connexion</h1>
-    </div>
-  )
+async function connecter() {
+  await signInWithEmailAndPassword(auth, email, motDePasse)
+}
+
+return (
+  <div>
+    <h1>Connexion</h1>
+
+    <input
+      type="email"
+      placeholder="Votre email"
+      value={email}
+      onChange={function(e) { setEmail(e.target.value) }}
+    />
+
+    <input
+      type="password"
+      placeholder="Votre mot de passe"
+      value={motDePasse}
+      onChange={function(e) { setMotDePasse(e.target.value) }}
+    />
+
+    <button onClick={connecter}>
+      Se connecter
+    </button>
+  </div>
+)
 }
