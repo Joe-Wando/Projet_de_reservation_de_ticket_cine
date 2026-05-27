a7fe75745ccaf24c9aab487d3d5d8b893bf89767
Joe-Wando <jonathan6wando@gmail.com>
2026-05-27 01:01:19 +0000

    Insertition de la base de donnée

---
+++ projet/src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHAr89_OxwfsuNh6RaNVUJ3RzyjSw0bIY",
+  authDomain: "cine-app-14a67.firebaseapp.com",
+  projectId: "cine-app-14a67",
+  storageBucket: "cine-app-14a67.firebasestorage.app",
+  messagingSenderId: "56094840520",
+  appId: "1:56094840520:web:0cccd62dfab447aa79f7fd"
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app) 
