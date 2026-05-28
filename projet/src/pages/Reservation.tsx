493b74d01f17ac448118fcc0104c98cc1f546843
Joe-Wando <jonathan6wando@gmail.com>
2026-05-28 01:17:28 +0000

    fix reservation typescript

--- projet/src/pages/Reservation.tsx
+++ projet/src/pages/Reservation.tsx
@@ -1,10 +1,9 @@
 import { useParams } from "react-router-dom"
 import films from "../films"
 import { useState } from "react"
-import { db } from "../firebase"
+import { db, auth } from "../firebase"
 import { collection, addDoc } from "firebase/firestore"
 import emailjs from "@emailjs/browser"
-import { auth } from '../firebase'
 
 export default function Reservation() {
   const { id } = useParams()
@@ -16,7 +15,6 @@ export default function Reservation() {
   const [reserve, setReserve] = useState(false)
   const [nom, setNom] = useState("")
   const [prenom, setPrenom] = useState("")
-  const [email, setEmail] = useState("")
 
   async function confirmerReservation() {
     await addDoc(collection(db, "reservations"), {
@@ -24,7 +22,7 @@ export default function Reservation() {
       filmTitre: film?.titre,
       nom: nom,
       prenom: prenom,
-      email: email,
+      email: auth.currentUser?.email,
       date: new Date().toLocaleDateString()
     })
 
@@ -34,7 +32,7 @@ export default function Reservation() {
       {
         nom: nom,
         prenom: prenom,
-        email: email,
+        email: auth.currentUser?.email,
         film_titre: film?.titre,
         date: new Date().toLocaleDateString()
       },
@@ -50,7 +48,7 @@ export default function Reservation() {
       <p>Vous avez choisi : {film?.titre}</p>
 
       {reserve ? (
-        <p>Réservation confirmée !</p>
+        <p> Réservation confirmée !</p>
       ) : (
         <div>
           <input
@@ -64,12 +62,6 @@ export default function Reservation() {
             placeholder="Votre prénom"
             value={prenom}
             onChange={function(e) { setPrenom(e.target.value) }}
-          />
-          <input
-            type="email"
-            placeholder="Votre email"
-            value={email}
-            onChange={function(e) { setEmail(e.target.value) }}
           />
           <button onClick={confirmerReservation}>
             Confirmer la réservation
